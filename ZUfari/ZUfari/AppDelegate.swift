33139d5bcae2f89b53562f699e1e27ac580042ef
Ye Wai Yan <bupstan.dev@gmail.com>
2019-07-14 13:43:22 +0630

    Create Cocoa App

---
+++ ZUfari/ZUfari/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ZUfari
+//
+//  Created by Ye Wai Yan on 7/14/19.
+//  Copyright © 2019 Ye Wai Yan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
